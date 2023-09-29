e35377d44ea40b1738fee7fca6a325fb0bb2be74
Ehijinwa <ehi20001@byui.edu>
2023-09-29 16:01:02 -0600

    prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magic number?");
+        string userInput = Console.ReadLine();
+        int magicNumber = int.Parse(userInput);
+        Console.WriteLine("What is your guess?");
+        userInput = Console.ReadLine();
+        int guess = int.Parse(userInput);
+        while (magicNumber != guess)
+        {
+            if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            Console.WriteLine("What is your guess?");
+            userInput = Console.ReadLine();
+            guess = int.Parse(userInput);
+        }
+        Console.WriteLine("Good Job!");
     }
 }
